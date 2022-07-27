2d19f9e00eae00db64530853abcfbfdb310467d4
AleksandrKrivoruchko <avk741120@yandex.ru>
2022-07-27 21:04:17 +0300

    Final version

--- homework6/Homework6/src/ExpressionCalc.java
+++ homework6/Homework6/src/ExpressionCalc.java
@@ -1,13 +1,59 @@
 // Задано уравнение вида q + w = e, q, w, e >= 0. Некоторые цифры могут быть заменены знаком вопроса,
 // например 2? + ?5 = 69. Требуется восстановить выражение до верного равенства.
 //  Предложить хотя бы одно решение или сообщить, что его нет.
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExpressionCalc {
     public static void main(String[] args) {
-        String strExpr = "9? + ?95 = ?93";
-        System.out.println(strExpr);
-        RestoreExpression re = new RestoreExpression(strExpr);
+        List<String> listExpr = new ArrayList<>();
+        listExpr.add("19? + ?95 = ?93");
+        listExpr.add("19? + ?55 = ?93");
+        listExpr.add("2? + ?5 = 69");
+        listExpr.add("2? + ?? = 6?");
+        listExpr.add("19? + ?95 = 93");
+        listExpr.add("2? + ?5 =- 69");
+        List<String> lsRes = testRestoreExpression(listExpr);
+        for (String s : lsRes) {
+            System.out.println(s);
+//            System.out.println();
+        }
+    }
 
-        System.out.println(re.isResultExpr());
+    static List<String> testRestoreExpression(List<String> ls) {
+        List<String> lsRes = new ArrayList<>();
+        String start = "Исходное уравнение:\n";
+        String res = "Решение:\n";
+        String err = "Ошибка в уравнении ";
+        for (String s : ls) {
+            if (isExpressionRight(s)) {
+                lsRes.add(start + s);
+                RestoreExpression tmp = new RestoreExpression(s);
+                lsRes.add(res + tmp.isResultExpr());
+            } else {
+                lsRes.add(start + s);
+                lsRes.add(err + s);
+            }
+        }
+        return lsRes;
+    }
+    static boolean isExpressionRight(String str) {
+        boolean plus = true;
+        boolean equally = true;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) continue;
+            if (str.charAt(i) == '?') continue;
+            if (plus && str.charAt(i) == '+') {
+                plus = false;
+                continue;
+            }
+            if (equally && str.charAt(i) == '=') {
+                equally = false;
+                continue;
+            }
+            if (str.charAt(i) == ' ') continue;
+            return false;
+        }
+        return true;
     }
 }
